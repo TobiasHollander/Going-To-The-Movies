5032b75486093aa3f38e0b9025159a8ce104bb6a
Tobias Hollander <tobiashollander@Tobiass-MacBook-Pro.local>
2018-05-28 16:00:33 -0500

    redesigned layout and added a function to display data to html div

--- js/main.js
+++ js/main.js
@@ -1,22 +1,32 @@
 $(document).ready(function(){
-  initIncludes();
-  $("#sumbit").click(getMovieData);
+
+  $("#submit").click(getMovieData);
 });
 
-function initIncludes(){
-  $("#nav-include").load('../includes/nav.html');
-}
-
 function getMovieData(){
-  var uri = "//www.omdbapi.com/";
+  var uri = "http://www.omdbapi.com/";
   $.ajax({
     type: "GET",
     url: uri,
     dataType: 'json',
     data: $("#f").serialize(),
     success: function(data){
+      outputData(data)
       console.log(data);
     }
   });
   return false;
+}
+
+function outputData(d){
+  var data = d.Search;
+  console.log(data);
+  var targetDiv = $("#movie-data-output");
+  targetDiv.empty();
+  for(var i = 0; i < data.length; i++){
+    var img = data[i].Poster
+    if(img === "N/A") img = "http://www.reelviews.net/resources/img/default_poster.jpg";
+    targetDiv.append("<div class='col s3'><div class='card small'><div class='card-image'><img src='"+ img +"'></div><div class='card-content'><h6>"+data[i].Title+"</h6></div></div>");
+  }
+
 }
